8807b5ee844e96cdd4e1c23b9417df86e1ce2022
Marlene Andre <andre.marlene1990@gmail.com>
2022-04-30 15:14:08 -0400

    adding routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :pokemons, only: [ :index, :show, :new, :create ] do
+    resources :bookings, only: [ :new, :create ]
+  end
+  resources :bookings do
+    resources :pokemon_reviews, only: [ :new, :create ]
+  end
 end
