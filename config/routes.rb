e182513b2caee42db14f217f1ac8a29be0547fe1
Marlene Andre <andre.marlene1990@gmail.com>
2022-04-30 16:28:27 -0400

    added custom routes approved by TA

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,25 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
   resources :pokemons, only: [ :index, :show, :new, :create ] do
     resources :bookings, only: [ :new, :create ]
   end
+
   resources :bookings, only: [] do
     resources :pokemon_reviews, only: [ :new, :create ]
   end
-  resources :users, only: [] do
-    resources :bookings, only: [ :index, :show ]
-    resources :pokemons, only: [ :index, :show ]
-  end
+
+  # Pokemon owners (Custom routes)
+  get '/my-pokemons', to: 'pokemons#my_pokemons', as: :my_pokemons
+
+  # Pokemon renter bookings (Custom routes)
+  get '/my-bookings', to: 'bookings#my_bookings', as: :my_bookings
+  get '/my-bookings/:id', to: 'bookings#show', as: :my_booking
+  delete '/my-bookings/:id', to: 'bookings#destroy'
+
+  # Owner bookings (Custom routes)
+  get '/owner-bookings', to: 'bookings#owner_bookings', as: :owner_bookings
+  get '/owner-bookings/:id', to: 'bookings#owner_booking', as: :owner_booking
+  patch '/owner-bookings/:id/set-status', to: 'bookings#set_status'
 end
