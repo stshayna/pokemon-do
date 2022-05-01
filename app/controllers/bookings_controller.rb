80c5eb373a1c652c1ebb85070fea1f3b1f36ad3c
Marlene Andre <andre.marlene1990@gmail.com>
2022-05-01 11:56:19 -0400

    adding new params

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -47,6 +47,6 @@ class BookingsController < ApplicationController
   end
 
   def booking_params
-    params.require(:booking).permit(:start_date, :end_date)
+    params.require(:booking).permit(:status, :start_date, :end_date)
   end
 end
