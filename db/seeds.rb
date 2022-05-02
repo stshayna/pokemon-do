c6efc6af2d8f81429dc89bd14760e057449fdd85
Marlene Andre <andre.marlene1990@gmail.com>
2022-05-02 19:54:18 -0400

    add pictures corresponding to pokemons name

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
 
 puts "Releasing Pokemons to the wild"
 puts ''
@@ -18,16 +19,29 @@ Pokemon.destroy_all
     password: '123456'
   )
   puts "Pokemon trainer #{@user.username} was born!"
+end
 
+10.times do
+  pokemon_name = Faker::Games::Pokemon.name
   @pokemon = Pokemon.create!(
     user_id: @user.id,
     name: Faker::Games::Pokemon.name,
     description: Faker::Games::Pokemon.move,
     location: Faker::Address.full_address,
-    price: rand(100..200)
+    price: rand(100..200),
+    image_url: "https://img.pokemondb.net/artwork/large/#{pokemon_name.downcase}.jpg"
   )
   puts "#{@user.username} just caught #{@pokemon.name}!"
 end
+
+puts "Creating bookings"
+@booking = Booking.create!(
+  user_id: User.first.id,
+  pokemon_id: Pokemon.second.id,
+  start_date: "2022-05-03",
+  end_date: "2022-05-05"
+)
+puts "#{User.first.username} just booked #{Pokemon.second.name}!"
 
 puts ''
 puts "Finished catching pokemons :)"
