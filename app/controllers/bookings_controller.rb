d86ba64a64dae4d10c7dfc05b614cacb6d44ea99
Marlene Andre <andre.marlene1990@gmail.com>
2022-05-03 21:19:12 -0400

    total booking added to history bookings page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,6 @@
 class BookingsController < ApplicationController
   before_action :find_pokemon, only: [ :new, :create ]
-  before_action :find_booking, only: [:show, :edit, :update]
+  before_action :find_booking, only: [:show, :edit, :update, :accept, :reject]
 
   def index
     @bookings = Booking.all
@@ -35,6 +35,17 @@ class BookingsController < ApplicationController
   #   @booking.destroy
   #   redirect_to pokemon_path(@bookings)
   # end
+
+  def accept
+    p 'accept booking'
+    @booking.update(status: "Accepted")
+    redirect_to my_bookings_history_path
+  end
+
+  def reject
+    @booking.update(status: "Rejected")
+    redirect_to my_bookings_history_path
+  end
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,20 +2,24 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :pokemons, only: [ :index, :show, :new, :create, :edit, :update ] do
-    # we need to nest our bookings inside pokemon
-    resources :bookings, only: [ :show, :new, :create ]
-  end
-
-  resources :bookings, only: [] do
-    resources :pokemon_reviews, only: [ :new, :create ]
-  end
-
-  resources :bookings, only: [:index, :edit, :update]
-
   # Pokemon owners (Custom routes)
   get '/my-pokemons', to: 'pages#my_pokemons', as: :my_pokemons
 
   # See my booking status as an owner & renter
   get '/my-bookings-history', to: 'pages#my_bookings_history'
+
+  # As an owner, I can change booking status to accept or reject
+  patch '/bookings/:id/accept', to: 'bookings#accept', as: :booking_accept
+  patch '/bookings/:id/reject', to: 'bookings#reject', as: :booking_reject
+
+  resources :pokemons, only: [ :index, :show, :new, :create, :edit, :update ] do
+    # we need to nest our bookings inside pokemon
+    resources :bookings, only: [ :show, :new, :create ]
+  end
+
+  resources :bookings do
+    resources :pokemon_reviews, only: [ :new, :create ]
+  end
+
+  resources :bookings, only: [:index, :edit, :update]
 end
